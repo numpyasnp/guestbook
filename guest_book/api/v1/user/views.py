4e85b2db4a50909354968e52717e0232c44b2aff
Ugurcan Ipek <ugurcann.ipek@gmail.com>
2025-09-13 22:48:09 +0300

    add cache for user list api view

--- guest_book/api/v1/user/views.py
+++ guest_book/api/v1/user/views.py
@@ -1,9 +1,12 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework.generics import ListAPIView
 
 from api.v1.user.serializers import UserSerializer
 from user.models import User
 
 
+@method_decorator(cache_page(60 * 5), name="get")
 class UserListAPIView(ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.with_entry_summary()
--- guest_book/guest_book/settings.py
+++ guest_book/guest_book/settings.py
@@ -90,6 +90,13 @@ DATABASES = {
     }
 }
 
+# settings.py
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

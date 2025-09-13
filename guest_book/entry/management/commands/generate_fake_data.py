97aab3767ee3c744d2228f8578ffcc457c5e1f09
Ugurcan Ipek <ugurcann.ipek@gmail.com>
2025-09-13 21:57:09 +0300

    add fake date command & add faker library for generate fake data

---
+++ guest_book/entry/management/commands/generate_fake_data.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from user.models import User
+from entry.models import Entry
+from faker import Faker
+import random
+
+
+class Command(BaseCommand):
+    help = "Generate fake Users and Entries with bulk_create"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--users", type=int, default=1000, help="Number of users to create")
+        parser.add_argument("--entries", type=int, default=10000, help="Number of entries to create")
+        parser.add_argument("--batch", type=int, default=1000, help="Batch size for bulk_create")
+
+    def handle(self, *args, **options):
+        fake = Faker()
+
+        users_count = options["users"]
+        entries_count = options["entries"]
+        batch_size = options["batch"]
+
+        self.stdout.write(self.style.WARNING(f"Creating {users_count} users..."))
+
+        users = [User(name=fake.unique.name()) for _ in range(users_count)]
+        User.objects.bulk_create(users, batch_size=batch_size)
+
+        self.stdout.write(self.style.SUCCESS(f"{users_count} users created."))
+
+        all_users = list(User.objects.all())
+        self.stdout.write(self.style.WARNING(f"Creating {entries_count} entries..."))
+
+        entries = []
+        for i in range(entries_count):
+            entries.append(
+                Entry(
+                    user=random.choice(all_users),
+                    subject=fake.sentence(nb_words=6),
+                    message=fake.paragraph(nb_sentences=5),
+                )
+            )
+            if len(entries) >= batch_size:
+                Entry.objects.bulk_create(entries, batch_size=batch_size)
+                entries = []
+
+        if entries:
+            Entry.objects.bulk_create(entries, batch_size=batch_size)
+
+        self.stdout.write(self.style.SUCCESS(f"{entries_count} entries created."))
